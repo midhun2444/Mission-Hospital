fb8fb99ec5c43f6c8f842a3ad5bbfedb47fad0d0
midhun2444 <github2444@gmail.com>
2026-08-09 15:02:24 +0530

    Update import paths in Doctors.jsx

--- Doctors.jsx
+++ Doctors.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Search, Building2, Star, ChevronDown } from "lucide-react";
-import GlassCard from "../components/GlassCard";
-import Reveal from "../components/Reveal";
-import { getDoctors, getDepartments } from "../services/api";
+import GlassCard from "./GlassCard";
+import Reveal from "./Reveal";
+import { getDoctors, getDepartments } from "./api";
 
 export default function Doctors({ onBook, presetDept }) {
   const [query, setQuery] = useState("");
