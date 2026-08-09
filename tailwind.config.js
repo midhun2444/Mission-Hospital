dfdc4f426c5bf1b5f93540c3186c71ef3b1c4afd
midhun2444 <github2444@gmail.com>
2026-08-09 15:25:42 +0530

    Update content paths in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+  "./index.html",
+  "./*.js",
+  "./*.jsx",
+  "./**/*.js",
+  "./**/*.jsx",
+],
   theme: {
     extend: {
       colors: {
